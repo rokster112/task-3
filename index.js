636ad4f71d3d592f9973213b519e6debaea80e09
rokas <rokster112@gmail.com>
2025-09-22 15:27:24 +0300

    t

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const http=require("http")
 http
   .createServer((req, res) => {
-    if (u.pathname !== "/rokster112_gmail_com") return res.end("NaN")
     const u=new URL(req.url, `http://${req.headers.host}`),
-      x=+u.searchParams.get("x"),
-      y=+u.searchParams.get("y")
+    x=+u.searchParams.get("x"),
+    y=+u.searchParams.get("y")
+    if (u.pathname !== "/rokster112_gmail_com") return res.end("NaN")
     if(!(x>0&&y>0&&x%1==0&&y%1==0)) return res.end("NaN")
     let m=Math.max(x, y)
     while (m%x||m%y) m++
