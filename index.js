f64fd7bd3329986e0473dadbdfd70dbe52d8f9b5
rokas <rokster112@gmail.com>
2025-09-22 16:24:15 +0300

    had to change it to a formula, because it breaks the server

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-const http=require("http")
+const http = require("http");
+
+function gcd(a, b) {
+  while (b) [a, b] = [b, a % b];
+  return a;
+}
+
+function lcm(a, b) {
+  return (a * b) / gcd(a, b);
+}
+
 http
   .createServer((req, res) => {
-    const u=new URL(req.url, `http://${req.headers.host}`),
-    x=+u.searchParams.get("x"),
-    y=+u.searchParams.get("y")
-    if (u.pathname !== "/rokster112_gmail_com") return res.end("NaN")
-    if(!(x>0&&y>0&&x%1==0&&y%1==0)) return res.end("NaN")
-    let m=Math.max(x, y)
-    while (m%x||m%y) m++
-    res.end(m+"")
-  }).listen(process.env.PORT || 3000)
+    const u = new URL(req.url, `http://${req.headers.host}`);
+    const x = +u.searchParams.get("x");
+    const y = +u.searchParams.get("y");
+
+    if (u.pathname !== "/rokster112_gmail_com") return res.end("NaN");
+    if (!(x > 0 && y > 0 && x % 1 === 0 && y % 1 === 0)) return res.end("NaN");
+
+    res.end(String(lcm(x, y)));
+  })
+  .listen(process.env.PORT || 3000);
