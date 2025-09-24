54d80e1c1f77188bd88d1774fd0e137bd53583a5
rokas <rokster112@gmail.com>
2025-09-24 15:38:16 +0300

    using BigInt to get full number

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
 const http = require("http");
 
 function gcd(a, b) {
-  while (b) [a, b] = [b, a % b];
+  while (b !== 0n) [a, b] = [b, a % b];
   return a;
 }
 
 function lcm(a, b) {
-    if (a === 0) return b;
-    if (b === 0) return a;
   return (a * b) / gcd(a, b);
 }
 
 http
   .createServer((req, res) => {
     const u = new URL(req.url, `http://${req.headers.host}`);
-    const x = +u.searchParams.get("x");
-    const y = +u.searchParams.get("y");
+    const x = BigInt(u.searchParams.get("x"));
+    const y = BigInt(u.searchParams.get("y"));
 
     if (u.pathname !== "/rokster112_gmail_com") return res.end("NaN");
-    if (!(x >= 0 && y >= 0 && x % 1 === 0 && y % 1 === 0)) return res.end("NaN");
+    if (x < 0n || y < 0n) return res.end("NaN");
 
-    res.end(String(lcm(x, y)));
+    res.end(lcm(x, y).toString());
   })
   .listen(process.env.PORT || 3000);
