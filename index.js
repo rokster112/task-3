074afcc07e371ef59b33360a593597bdec673d17
rokas <rokster112@gmail.com>
2025-09-24 15:48:30 +0300

    s

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-const http = require("http");
-
+const http = require("http")
 function gcd(a, b) {
-  while (b !== 0n) [a, b] = [b, a % b];
-  return a;
+  while (b !== 0n) [a, b] = [b, a % b]
+  return a
 }
-
 function lcm(a, b) {
-  return (a * b) / gcd(a, b);
+  return (a * b) / gcd(a, b)
 }
-
 http
   .createServer((req, res) => {
-    const u = new URL(req.url, `http://${req.headers.host}`);
-    const x = BigInt(u.searchParams.get("x"));
-    const y = BigInt(u.searchParams.get("y"));
+    const u = new URL(req.url, `http://${req.headers.host}`)
+    const xs = u.searchParams.get("x"),
+      ys = u.searchParams.get("y")
+    if (xs == null || ys == null) return res.end("NaN")
+    const x = BigInt(xs),
+      y = BigInt(ys)
 
-    if (u.pathname !== "/rokster112_gmail_com") return res.end("NaN");
-    if (x < 0n || y < 0n) return res.end("NaN");
-
-    res.end(lcm(x, y).toString());
+    if (u.pathname !== "/rokster112_gmail_com") return res.end("NaN")
+    if (x < 0n || y < 0n) return res.end("NaN")
+    res.end(lcm(BigInt(x), BigInt(y)).toString())
   })
-  .listen(process.env.PORT || 3000);
+  .listen(process.env.PORT || 3000)
