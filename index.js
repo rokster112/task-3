4d512da8136ef6fdccbc58e6915516ae0182a96c
rokas <rokster112@gmail.com>
2025-09-24 16:01:08 +0300

    s

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function gcd(a, b) {
   return a
 }
 function lcm(a, b) {
-  if (a === 0 && b === 0) return 0
+  if (a === 0n && b === 0n) return 0n;
   return (a * b) / gcd(a, b)
 }
 http
@@ -18,6 +18,6 @@ http
 
     if (u.pathname !== "/rokster112_gmail_com") return res.end("NaN")
     if (x < 0n || y < 0n) return res.end("NaN")
-    res.end(lcm(BigInt(x), BigInt(y)).toString())
+    res.end(lcm(x, y).toString())
   })
   .listen(process.env.PORT || 3000)
